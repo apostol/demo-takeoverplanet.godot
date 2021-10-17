ce4749052dde636ec099812500fd7b145443d5bc
apostol <developer@dpankratov.ru>
2021-10-17 22:20:57 +0400

    Bug with scene loading is fixed

--- src/UI/ChangeSceneButton.gd
+++ src/UI/ChangeSceneButton.gd
@@ -4,6 +4,7 @@ extends Button
 export(String, FILE) var next_scene_path:= ""
 
 func _on_button_up():
+	get_tree().paused = false
 	var error = get_tree().change_scene(next_scene_path)
 	print_debug(error)
 	
--- src/UI/QuitButton.gd
+++ src/UI/QuitButton.gd
@@ -1,4 +1,5 @@
 extends Button
 
 func _on_button_up():
+	get_tree().paused = false
 	get_tree().quit()
